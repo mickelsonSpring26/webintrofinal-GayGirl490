32f969743cf094405535870c9731138a17d629e9
GayGirl490 <astrid.cranney@students.snow.edu>
2026-04-14 14:07:29 -0600

    href change instead of replace

--- docs/files/loginui.js
+++ docs/files/loginui.js
@@ -4,28 +4,26 @@ import { GetGSFlags } from "./service.js";
 // const url = new URL(window.location);
 
 // if (window.location.pathname.includes("/login.html")) {
-  // RenderPage();
+// RenderPage();
 
-  const selectFileFormElement = dgebi("select-file-form");
-  selectFileFormElement.addEventListener("submit", async (e) => {
-    e.preventDefault();
+const selectFileFormElement = dgebi("select-file-form");
+selectFileFormElement.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-    const saveFilePathInput = dgebi("save-file-path");
-    const fileNumberInput = dgebi("file-number");
-    const filePathToSend = `${saveFilePathInput.value}/file${fileNumberInput.value}.sav`;
+  const saveFilePathInput = dgebi("save-file-path");
+  const fileNumberInput = dgebi("file-number");
+  const filePathToSend = `${saveFilePathInput.value}/file${fileNumberInput.value}.sav`;
 
-    console.log(
-      `file ${fileNumberInput.value}`,
-      await GetGSFlags(filePathToSend),
-    );
+  console.log(
+    `file ${fileNumberInput.value}`,
+    await GetGSFlags(filePathToSend),
+  );
 
-    const spoilerSelectInput = dgebi("spoiler");
-    //   url.searchParams.set("spoiler", spoilerSelectInput.value);
-    //   history.pushState(null, "", url); // What was this for again?
+  const spoilerSelectInput = dgebi("spoiler");
+  //   url.searchParams.set("spoiler", spoilerSelectInput.value);
+  //   history.pushState(null, "", url); // What was this for again?
 
-    window.location.replace(
-      `http://127.0.0.1:5500/index.html?spoiler=${spoilerSelectInput.value}`,
-    );
-    // e.target.reset();
-  });
+  window.location.href = `http://127.0.0.1:5500/index.html?spoiler=${spoilerSelectInput.value}`;
+  // e.target.reset();
+});
 // }
