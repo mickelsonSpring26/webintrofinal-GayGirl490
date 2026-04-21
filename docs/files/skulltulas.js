52fe5b270d72fcc5d44fcc8b110847949233e8f7
GayGirl490 <astrid.cranney@students.snow.edu>
2026-04-21 11:30:44 -0600

    dockerfile

--- docs/files/skulltulas.js
+++ docs/files/skulltulas.js
@@ -1,4 +1,6 @@
 // For these, I'm just going to Ctrl+C, Ctrl+V from https://www.zeldadungeon.net/wiki/Ocarina_of_Time_Gold_Skulltulas to save time on writing descriptions.
+// time of day: 1(01) = night; 2(10) = day; 3(11) = both;
+// era: 1(01) = past; 2(10) = future; 3(11) = both;
 
 const skulltulaData = [
   {
@@ -7,6 +9,8 @@ const skulltulaData = [
     area: "dekuTree",
     specificArea: "Deku Tree",
     index: 4,
+    timeOfDay: 3,
+    era: 1,
     imageURL: "./files/Skulltula images/OoT-Gold-Skulltula-001.jpg",
     description:
       "In the same room with Compass, raise the platforms and use them to get onto the platform on the left side. The Skulltula is right behind the treasure chest.",
@@ -17,8 +21,11 @@ const skulltulaData = [
     area: "dekuTree",
     specificArea: "Deku Tree",
     index: 5,
+    timeOfDay: 3,
+    era: 1,
     imageURL: "./files/Skulltula images/OoT-Gold-Skulltula-002.jpg",
-    description: "After breaking through the spider web and reaching the lower floor of the Great Deku Tree, turn back and look at the vines leading up. Shoot down the Skulltula with the Slingshot.",
+    description:
+      "After breaking through the spider web and reaching the lower floor of the Great Deku Tree, turn back and look at the vines leading up. Shoot down the Skulltula with the Slingshot.",
   },
   {
     number: 3,
@@ -26,8 +33,11 @@ const skulltulaData = [
     area: "dekuTree",
     specificArea: "Deku Tree",
     index: 6,
+    timeOfDay: 3,
+    era: 1,
     imageURL: "./files/Skulltula images/OoT-Gold-Skulltula-003.jpg",
-    description: "After breaking through the spider web and reaching the lower floor of the Great Deku Tree, look at the grating at the north end of the room to find another Skulltula.",
+    description:
+      "After breaking through the spider web and reaching the lower floor of the Great Deku Tree, look at the grating at the north end of the room to find another Skulltula.",
   },
   {
     number: 45,
@@ -35,6 +45,8 @@ const skulltulaData = [
     area: "kakarikoVillage",
     specificArea: "Kakariko Village",
     index: 1,
+    timeOfDay: 1,
+    era: 2,
     imageURL: "./files/Skulltula images/OoT-Gold-Skulltula-045.jpg",
     description:
       "The Gold Skulltula is located on top of Impa's House in Kakariko Village. Make your way to the south end of Kakariko Village, next to Impa's House. From here, Hookshot onto the roof of the Skulltula House and then Hookshot onto the roof of Impa's House. At nighttime, you will find a Gold Skulltula located here.",
